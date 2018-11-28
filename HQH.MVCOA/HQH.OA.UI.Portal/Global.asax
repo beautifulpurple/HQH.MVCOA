cf1c91b3db98252448b8c07a5383002c8df0ca4b
黄强华 <huangqianghua@outlook.com>
2018-11-28 22:02:48 +0800

    create framework

---
+++ HQH.MVCOA/HQH.OA.UI.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HQH.OA.UI.Portal.MvcApplication" Language="C#" %>
---
+++ HQH.MVCOA/HQH.OA.UI.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HQH.OA.UI.Portal.Startup))]
+namespace HQH.OA.UI.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
