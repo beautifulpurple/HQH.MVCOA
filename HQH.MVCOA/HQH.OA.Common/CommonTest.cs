44a487c9228c77c1dbb8a03d5df163c90c4a62fd
黄强华 <huangqianghua@outlook.com>
2018-11-29 15:43:00 +0800

    test add

---
+++ HQH.MVCOA/HQH.OA.Common/CommonTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HQH.OA.Common
+{
+    class CommonTest
+    {
+        //这是一个测试文件
+    }
+}
