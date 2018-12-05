ef2ea5faa1715915cc1e1cc45d8ab7b2eae8fbfe
黄强华 <huangqianghua@outlook.com>
2018-12-05 23:10:19 +0800

    12121

--- HQH.MVCOA/HQH.OA.BLL/BaseService.cs
+++ HQH.MVCOA/HQH.OA.BLL/BaseService.cs
@@ -1,11 +1,11 @@
-﻿using HQH.OA.DalFactory;
-using HQH.OA.IDAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using HQH.OA.DalFactory;
+using HQH.OA.IDAL;
 
 namespace HQH.OA.BLL
 {
