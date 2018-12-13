7b0b5bc40c98708cc14fcab5daf745723f073523
黄强华 <huangqianghua@outlook.com>
2018-12-13 18:35:23 +0800

    IDal.tt

---
+++ HQH.MVCOA/HQH.OA.IDAL/IDal.cs
@@ -0,0 +1,21 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HQH.OA.Model;
+namespace HQH.OA.IDAL
+{
+   
+		
+	public partial interface IOrderInfoDal : IBaseDal<OrderInfo>
+    {
+	}   
+		
+	public partial interface IUserInfoDal : IBaseDal<UserInfo>
+    {
+	}   
+
+
+}
