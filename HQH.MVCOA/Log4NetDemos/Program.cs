c2c50f8ebb05eb65ed7d38ffa6f9ec3e817e8072
黄强华 <huangqianghua@outlook.com>
2018-12-14 18:48:29 +0800

    start log4net

---
+++ HQH.MVCOA/Log4NetDemos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //http://www.cnblogs.com/android-blogs/p/6038282.html
+            //https://www.cnblogs.com/vichin/p/6022612.html
+            //https://www.cnblogs.com/lsgsanxiao/p/5845300.html
+        }
+    }
+}
